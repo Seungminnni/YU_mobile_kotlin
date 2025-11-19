c0c09c88fe5e874332142c93456eb80574a74698
Lee-SeungMin <51289006+Seungminnni@users.noreply.github.com>
2025-11-19 18:27:09 +0900

    모두 삭제 후 재업로드 예정

--- app/src/test/java/com/example/a1/HelloWorldTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-
-class HelloWorldTest {
-    @Test
-    fun testHelloWorld() {
-        assertEquals("Hello, World!", HelloWorld.greet())
-    }
-}
